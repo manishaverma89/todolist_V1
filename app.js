5cef44771c633bde09edae3e2b4cdc54b6e9ec5d
manisha <manisha.verma23@gmail.com>
2022-03-15 18:15:22 +0530

    Usng Javascript Date objects, new Date() will give the current date.

--- app.js
+++ app.js
@@ -5,9 +5,19 @@ const bodyParser = require("body-parser");
 const app = express();                // app is an instance of express
 
 
-app.get("/",function(req,res){
-    res.send("hello");       //this is what the server will response to the browser(or the user)at 
-});                          // this route ("/")
+app.get("/", function(req,res){
+
+    var today = new Date();      //Javascript Date objects, new Date() will give the current date.
+                                 
+
+ if (today.getDay()===0 || today.getDay===6){
+     console.log("wow! Its a weekend");
+ } else{
+     res.send("God! I have to work");
+ }
+    
+
+});                          
 
 
 
