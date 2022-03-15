d5547d7759c7ae26d7562e8a3a62d3371525cde3
manisha <manisha.verma23@gmail.com>
2022-03-15 15:33:03 +0530

    starting files

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 // Starting files
 const express = require("express");
-const https = require('https') ;      // to make https request
-
 const bodyParser = require("body-parser");
+
 const app = express();                // app is an instance of express
 
 
 app.get("/",function(req,res){
-    res.send("hello");         //this is what the server will response to the browser(or the user)at "/"
-});
+    res.send("hello");       //this is what the server will response to the browser(or the user)at 
+});                          // this route ("/")
 
 
 
