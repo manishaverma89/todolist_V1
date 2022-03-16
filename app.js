e568ec7d972332a067df2df3c0f618ae55240fcd
manisha <manisha.verma23@gmail.com>
2022-03-16 20:53:54 +0530

    Created first EJS template, added list.ejs file in views folder

--- app.js
+++ app.js
@@ -4,23 +4,47 @@ const bodyParser = require("body-parser");
 
 const app = express();                // app is an instance of express
 
+app.set('view engine', 'ejs');
 
 app.get("/", function(req,res){
 
     var today = new Date();      //Javascript Date objects, new Date() will give the current date.
-                                 
+    var currentDay = today.getDay();
+    var day = "";                            
 
- if (today.getDay()===0 || today.getDay===6){
-     req.write("wow! Its a weekend");
- } else{
-    res.sendFile(__dirname + "/index.html");
- }
-    
+switch (currentDay) {
+    case 0: 
+       day = "Sunday";        
+        break;
+    case 1: 
+       day = "Monday";        
+        break;
+    case 2: 
+        day = "Tuesday";        
+        break;
+    case 3: 
+        day = "wednesday";        
+        break;
+    case 4: 
+        day = "Thursday";        
+        break;
+    case 5: 
+        day = "friday";        
+        break;
+    case 6: 
+        day = "Saturday";        
+        break;
+       
+    default:
+        console.log("Error:current day is equal to: "+ currentDay );
+        break;
+}
 
-});     
+    res.render("list", {kindOfDay:day});  
+    // Here we're passing the value of "day" into a variable "kindOfDay" which has to match the variable we added in list.ejs file marker "<%= kindOfDay %>".
+    // So this line will replace the value of entire marker with the value of "day".
 
-// res.sendFile() is used to send a html file.
-
+});  
 
 
 
