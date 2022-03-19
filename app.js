0a4ed730a9e93670f066422ce3a0a727bd4c123f
manisha <manisha.verma23@gmail.com>
2022-03-19 07:59:17 +0530

    converted day number to toLocaleString method and got value as a string

--- app.js
+++ app.js
@@ -8,41 +8,17 @@ app.set('view engine', 'ejs'); //This line of code tells our app which is genera
 app.get("/", function(req,res){
 
     var today = new Date();      //Javascript Date objects, new Date() will give the current date.
-    var currentDay = today.getDay();
-    var day = "";                            
+    
+                          
+    var options={
+        weekday:"long",
+        day:"numeric",
+        month:"long"
+    };
 
-switch (currentDay) {
-    case 0: 
-       day = "Sunday";        
-        break;
-    case 1: 
-       day = "Monday";        
-        break;
-    case 2: 
-        day = "Tuesday";        
-        break;
-    case 3: 
-        day = "wednesday";        
-        break;
-    case 4: 
-        day = "Thursday";        
-        break;
-    case 5: 
-        day = "friday";        
-        break;
-    case 6: 
-        day = "Saturday";        
-        break;
-       
-    default:
-        console.log("Error:current day is equal to: "+ currentDay );
-        break;
-}
-
+    var day= today.toLocaleString("en-US",options);    
     res.render("list", {kindOfDay:day});  
-    // Here we're passing the value of "day" into a variable "kindOfDay" which has to match the variable we added in list.ejs file marker "<%= kindOfDay %>".
-    // So this line will replace the value of entire marker with the value of "day".
-
+   
 });  
 
 
