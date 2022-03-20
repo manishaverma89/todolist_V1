cf0d09a60714ff2b9091b8b82a8fd1576c8cb7fa
manisha <manisha.verma23@gmail.com>
2022-03-20 19:22:18 +0530

    passing data through from the server

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ const bodyParser = require("body-parser");
 
 const app = express();                // app is an instance of express
 
+var items= ["buy food","cook food","Eat Food"];
 app.set('view engine', 'ejs'); //This line of code tells our app which is generated using express to use ejs as its "view engine". 
+
+app.use(bodyParser.urlencoded({extended:true})); 
+
+
 
 app.get("/", function(req,res){
 
@@ -17,9 +22,16 @@ app.get("/", function(req,res){
     };
 
     var day= today.toLocaleString("en-US",options);    
-    res.render("list", {kindOfDay:day});  
+    res.render("list", {kindOfDay:day , newListItems: items});  
    
 });  
+
+app.post("/",function(req,res){
+    var item = req.body.itemName;
+    items.push(item);
+    // console.log("Item Name is:"+ item);
+    res.redirect("/");
+});
 
 
 
