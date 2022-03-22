44d06eb77eac4f756a2899fbd4f357431483e6ad
manisha <manisha.verma23@gmail.com>
2022-03-22 18:36:19 +0530

    Added css in public folder in app.js

--- app.js
+++ app.js
@@ -3,31 +3,31 @@ const bodyParser = require("body-parser");
 
 const app = express();                // app is an instance of express
 
-var items= ["buy food","cook food","Eat Food"];
+let items= ["buy food","cook food","Eat Food"];
 app.set('view engine', 'ejs'); //This line of code tells our app which is generated using express to use ejs as its "view engine". 
 
 app.use(bodyParser.urlencoded({extended:true})); 
-
+app.use(express.static("public"));      //created to add sattic files like css,javascript,images
 
 
 app.get("/", function(req,res){
 
-    var today = new Date();      //Javascript Date objects, new Date() will give the current date.
+    let today = new Date();      //Javascript Date objects, new Date() will give the current date.
     
                           
-    var options={
+    let options={
         weekday:"long",
         day:"numeric",
         month:"long"
     };
 
-    var day= today.toLocaleString("en-US",options);    
+    let day= today.toLocaleString("en-US",options);    
     res.render("list", {kindOfDay:day , newListItems: items});  
    
 });  
 
 app.post("/",function(req,res){
-    var item = req.body.itemName;
+    let item = req.body.itemName;
     items.push(item);
     // console.log("Item Name is:"+ item);
     res.redirect("/");
