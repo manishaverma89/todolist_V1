bf015c30937cec6ece63b81a41208e2ff1a5ad42
manisha <manisha.verma23@gmail.com>
2022-03-23 16:55:38 +0530

    Added Another Route ie. a Work-List Page

--- app.js
+++ app.js
@@ -1,42 +1,51 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const app = express();                // app is an instance of express
+const app = express(); // app is an instance of express
 
-let items= ["buy food","cook food","Eat Food"];
-app.set('view engine', 'ejs'); //This line of code tells our app which is generated using express to use ejs as its "view engine". 
+let items = ["buy food", "cook food", "Eat Food"];
+let workItems = [];
+app.set("view engine", "ejs"); //This line of code tells our app which is generated using express to use ejs as its "view engine".
 
-app.use(bodyParser.urlencoded({extended:true})); 
-                     
-app.use(express.static("public"));      //public folder is created to add sattic files like css,javascript,images
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(express.static("public")); //public folder is created to add sattic files like css,javascript,images
 
-app.get("/", function(req,res){
+app.get("/", function (req, res) {
+  let today = new Date(); //Javascript Date objects, new Date() will give the current date.
 
-    let today = new Date();      //Javascript Date objects, new Date() will give the current date.
-    
-                          
-    let options={
-        weekday:"long",
-        day:"numeric",
-        month:"long"
-    };
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
 
-    let day= today.toLocaleString("en-US",options);    
-    res.render("list", {kindOfDay:day , newListItems: items});  
+  let day = today.toLocaleString("en-US", options);
+  res.render("list", { listTitle: day, newListItems: items });
+});
+
+app.post("/", function (req, res) {
    
-});  
+  let item = req.body.newItem;
+  if(req.body.list === "Work-List"){
+    workItems.push(item);
+    res.redirect("/work")
+  }else{
+      items.push(item);
+      res.redirect("/");
+  }
+  // console.log("Item Name is:"+ item);
+  
+});
 
-app.post("/",function(req,res){
-    let item = req.body.itemName;
-    items.push(item);
-    // console.log("Item Name is:"+ item);
-    res.redirect("/");
+// Work List
+
+app.get("/work", function (req, res) {
+  res.render("list", {listTitle: "Work-List" , newListItems:workItems});
+
 });
 
 
-
-app.listen(3000 , function(){
-
-    console.log("Server started at port 3000");
+app.listen(3000, function () {
+  console.log("Server started at port 3000");
 });
